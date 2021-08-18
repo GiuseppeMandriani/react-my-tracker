61acfe40951da8e0961dfcc7cf22e48fcf0d0e1d
Giuseppe Mandriani <giuseppe.mandriani@gmail.comgit config --global user.name Giuseppe>
2021-08-18 11:17:25 +0200

    add date in component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,20 @@
 import './ExpenseItem.css';
 
-const ExpenseItem = (props) => {
+const ExpenseItem = props => {
     // Js
 
-
+    const month = props.date.toLocaleDateString('it-IT', { month: 'long' });
+    const day = props.date.toLocaleDateString('it-IT', { day: '2-digit' });
+    const year = props.date.getFullYear();
 
     return (
         <div className="expense-item">
-            <div>{props.date.toISOString()}</div>
+            <div>
+                <div>{day}</div>
+                <div>{month.toUpperCase()}</div>
+                <div>{year}</div>
+            </div>
+
             <div className="expense-item__description">
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">€ {props.amount}</div>
