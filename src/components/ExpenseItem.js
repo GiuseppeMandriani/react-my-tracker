afadb1f29798eb0c9d803c8c82444df6d8cfd7cf
Giuseppe Mandriani <giuseppe.mandriani@gmail.comgit config --global user.name Giuseppe>
2021-08-18 10:27:46 +0200

    add js in component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,18 @@
 import './ExpenseItem.css';
 
 const ExpenseItem = () => {
+    // Js
+
+    const expenseDate = new Date(2021,7,18);
+    const expenseTitle = 'Car Insurance';
+    const expenseAmount = 250.9;
+
     return (
         <div className="expense-item">
-            <div>Date</div>
+            <div>{expenseDate.toISOString()}</div>
             <div className="expense-item__description">
-                <h2>Title</h2>
-                <div className="expense-item__price">Amount</div>
+                <h2>{expenseTitle}</h2>
+                <div className="expense-item__price">€ {expenseAmount}</div>
             </div>
         </div>
     );
