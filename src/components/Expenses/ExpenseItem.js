e621b14757b7b1435880fdf66e452d885d4cf157
Giuseppe Mandriani <giuseppe.mandriani@gmail.comgit config --global user.name Giuseppe>
2021-08-19 11:58:10 +0200

    add first event

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,21 @@
 import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = props => {
+
+    const clickHendler = () => {
+        console.log('Clicked');
+    }
     return (
-        <div className="expense-item">
+        <Card className="expense-item">
             <ExpenseDate date={props.date} />
             <div className="expense-item__description">
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">€ {props.amount}</div>
             </div>
-        </div>
+            <button onClick={clickHendler} className="">Change Title</button>
+        </Card>
     );
 };
 
